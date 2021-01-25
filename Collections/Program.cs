bba803055700e8cec077baaea4f75ef21163ea83
buraktaha marangoz <348420@ogr.ktu.edu.tr>
2021-01-25 21:27:34 +0300

    .

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] {"Ahmet", "Burak", "Veli", "Osman" };
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            //new
+        }
+    }
+}
